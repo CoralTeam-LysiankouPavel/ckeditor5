1767cbf6aa9aceee40efff7320fb782791b8ef6f
Pavel Lysiankou <pavel.lysiankou@check24.de>
2022-04-18 10:54:18 +0200

    issue-62: add description for insert banner plugin

--- src/insert-banner/plugin.js
+++ src/insert-banner/plugin.js
@@ -1,3 +1,22 @@
+/**
+ * - This plugin is used to provide functionality to insert banners (specific images).
+ * - These banners (array of objects) should be configured in editor config. (@see src/ckeditor.js `toolbar.app.banners` section).
+ *   By default it's empty array.
+ *
+ * - Banners (objects) format:
+ *   [
+ *     {
+ *         name: 'name_1',
+ *         url: 'url_1'
+ *     },
+ *     {
+ *         name: 'name_2',
+ *         url: 'url_2'
+ *     },
+ *     ...
+ *   ]
+ */
+
 import { Plugin } from 'ckeditor5/src/core';
 
 import InsertBannerEditing from "./insert-banner-editing";
