f824936490176e19609fa072c3de0ebcd796b18d
Pavel Lysiankou <pavel.lysiankou@check24.de>
2022-04-26 09:37:52 +0200

    issue-84: remove TableBorderPlugin, add border-collapse: collapse to table during downcast process

--- src/add-border-to-table-cell/converter.js
+++ src/add-border-to-table-cell/converter.js
@@ -5,6 +5,10 @@ export function modelAttributeToViewStyle(evt, data, conversionApi, border) {
     const figureViewElement = conversionApi.mapper.toViewElement(data.item);
     const viewWriter = conversionApi.writer;
 
+    for (const childViewElement of getChildrenByName(figureViewElement, 'table')) {
+        viewWriter.setStyle('border-collapse', 'collapse', childViewElement);
+    }
+
     for (const childViewElement of getChildrenByName(figureViewElement, 'td')) {
         viewWriter.setStyle('border', border, childViewElement);
     }
