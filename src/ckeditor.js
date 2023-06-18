9808254140e022f0282fd8d90bc0c8ccc50d05f6
pavel.lysiankou <pavel.lysiankou@coral.team>
2023-06-18 09:36:04 +0200

    * Update package version * comment custom plugins

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -55,17 +55,17 @@ import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapte
 
 
 // custom plugins
-import ImageDataDowncastPlugin from "./image-data-downcast/plugin";
-import AddPuddingToTableCellPlugin from "./add-padding-to-table-cell/plugin";
-import AddStyleOnImageAlignPlugin from './add-style-on-image-align/plugin';
-import AddStyleOnImageInsertPlugin from './add-style-on-image-insert/plugin';
-import AddStyleOnImageResizedPlugin from "./add-style-on-image-resized/plugin";
-import AddStyleOnTableInsertPlugin from  "./add-style-on-table-insert/plugin";
-import ExtendParagraphSchemaPlugin from "./extend-paragraph-schema/plugin";
-import InsertBannerPlugin from "./insert-banner/plugin";
-import InsertFooterPlugin from "./insert-footer/plugin";
-import TableWidthPlugin from "./table-width/plugin";
-import TableAlignPlugin from "./table-align/plugin";
+// import ImageDataDowncastPlugin from "./image-data-downcast/plugin";
+// import AddPuddingToTableCellPlugin from "./add-padding-to-table-cell/plugin";
+// import AddStyleOnImageAlignPlugin from './add-style-on-image-align/plugin';
+// import AddStyleOnImageInsertPlugin from './add-style-on-image-insert/plugin';
+// import AddStyleOnImageResizedPlugin from "./add-style-on-image-resized/plugin";
+// import AddStyleOnTableInsertPlugin from  "./add-style-on-table-insert/plugin";
+// import ExtendParagraphSchemaPlugin from "./extend-paragraph-schema/plugin";
+// import InsertBannerPlugin from "./insert-banner/plugin";
+// import InsertFooterPlugin from "./insert-footer/plugin";
+// import TableWidthPlugin from "./table-width/plugin";
+// import TableAlignPlugin from "./table-align/plugin";
 
 export default class ClassicEditor extends ClassicEditorBase {}
 
@@ -120,17 +120,17 @@ ClassicEditor.builtinPlugins = [
 	UploadAdapter,
 
 	// custom plugins
-	ImageDataDowncastPlugin,
-	AddPuddingToTableCellPlugin,
-	AddStyleOnImageAlignPlugin,
-	AddStyleOnImageInsertPlugin,
-	AddStyleOnImageResizedPlugin,
-	AddStyleOnTableInsertPlugin,
-	ExtendParagraphSchemaPlugin,
-	InsertBannerPlugin,
-	InsertFooterPlugin,
-	TableWidthPlugin,
-	TableAlignPlugin
+	// ImageDataDowncastPlugin,
+	// AddPuddingToTableCellPlugin,
+	// AddStyleOnImageAlignPlugin,
+	// AddStyleOnImageInsertPlugin,
+	// AddStyleOnImageResizedPlugin,
+	// AddStyleOnTableInsertPlugin,
+	// ExtendParagraphSchemaPlugin,
+	// InsertBannerPlugin,
+	// InsertFooterPlugin,
+	// TableWidthPlugin,
+	// TableAlignPlugin
 ];
 
 // Editor configuration.
